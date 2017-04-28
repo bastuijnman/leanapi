9b3d9545217d9742e930363d613e4c72035915b1
Bas Tuijnman <bas.tuijnman@24i.com>
2017-04-28 19:11:25 +0200

    Fix schema visuals when ENUM is in play

--- src/build.js
+++ src/build.js
@@ -2,7 +2,6 @@
 
 const browserify = require('browserify');
 const browserifyCssx = require('browserify-cssx');
-const browserifyShim = require('browserify-shim');
 const browserifyCss = require('browserify-css');
 const babelify = require('babelify');
 
@@ -16,13 +15,21 @@ module.exports = {
      * @returns {fs.ReadStream} - The read file stream.
      */
     getJsBuildStream: function (filename) {
-        return browserify(filename)
+        return browserify(filename, {
+            noParse: [
+                require.resolve(__dirname + '/../node_modules/json-schema-view-js/bower_components/json-formatter-js/dist/bundle.js'),
+                require.resolve(__dirname + '/../node_modules/json-schema-view-js/dist/bundle.min.js')
+            ]
+        })
+            .require([
+                require.resolve(__dirname + '/../node_modules/json-schema-view-js/bower_components/json-formatter-js/dist/bundle.js'),
+                require.resolve(__dirname + '/../node_modules/json-schema-view-js/dist/bundle.min.js')
+            ])
             .transform(browserifyCss)
             .transform(browserifyCssx)
             .transform(babelify, {
                 presets: ['es2015', 'react']
             })
-            .transform(browserifyShim)
             .bundle();
     }
 
--- src/components/resource/call/responses/schema.js
+++ src/components/resource/call/responses/schema.js
@@ -3,7 +3,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import JSONSchemaView from 'json-schema-view-js';
+// Looks awful. Couldn't find a better way to do it at the moment, but there should be a better way
+import './../../../../../node_modules/json-schema-view-js/bower_components/json-formatter-js/dist/bundle.js';
+import JSONSchemaView from './../../../../../node_modules/json-schema-view-js/dist/bundle.min.js';
 
 export default class Schema extends React.Component {
 
