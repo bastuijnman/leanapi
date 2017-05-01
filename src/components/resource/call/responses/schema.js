73de3b94dccab4443d8a0651d9a72287aef2a030
Bas Tuijnman <bas.tuijnman@24i.com>
2017-05-01 11:02:48 +0200

    Make sure the JSON Schema Dialog is always centered

--- src/components/resource/call/responses/schema.js
+++ src/components/resource/call/responses/schema.js
@@ -26,6 +26,17 @@ export default class Schema extends React.Component {
 
         let node = ReactDOM.findDOMNode(this);
 
+        /*
+         * We want to fire a resize event to toggle Dialog positioning.
+         * This is a bit dirty, but there are no methods exposed on the
+         * Dialog component to reposition it currently.
+         */
+        let toggleHandler = JSONSchemaView.prototype.toggle;
+        JSONSchemaView.prototype.toggle = function () {
+            toggleHandler.call(this);
+            window.dispatchEvent(new Event('resize'));
+        };
+
         if (node) {
             node.appendChild((new JSONSchemaView(jsonSchema, 1)).render())
         }
