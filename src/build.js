986aced1103fa4057738163a84cada29f7a84b4f
Bas Tuijnman <bas.tuijnman@24imedia.com>
2019-05-20 17:13:21 +0200

    Remove old css loader

--- src/build.js
+++ src/build.js
@@ -43,10 +43,6 @@ module.exports = {
                             }
                         }
                     },
-                    {
-                        test: /\.css$/,
-                        use: [ 'style-loader', 'css-loader' ]
-                    }
                 ]
             },
             plugins: [
